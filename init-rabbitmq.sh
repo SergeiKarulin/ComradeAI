d0e8482e823bb5750a22e537640de23eadbe4910
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-09 21:34:15 +0400

    One more try

--- init-rabbitmq.sh
+++ init-rabbitmq.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sleep 10
+# Function to wait for RabbitMQ server to start
+wait_for_rabbitmq() {
+    echo "Waiting for RabbitMQ to start..."
+    while ! rabbitmqctl status >/dev/null 2>&1; do
+        sleep 1
+    done
+}
+
+# Wait for RabbitMQ server to start
+wait_for_rabbitmq
+
+# Add vhost, user, and set permissions
 rabbitmqctl add_vhost demoAccess
 rabbitmqctl add_user remoteClient01 remoteClient01Pass
 rabbitmqctl set_permissions -p demoAccess remoteClient01 ".*" ".*" ".*"
