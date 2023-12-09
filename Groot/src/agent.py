3a0d65c3c0d2e9f6607669cdb71a462169299003
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-09 20:01:05 +0400

    Introducing Groot

---
+++ Groot/src/agent.py
@@ -0,0 +1,26 @@
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='RabbitMQCluster01'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+def Reply():
+    return "I am Groot."
+
+def on_request(ch, method, props, body):
+    response = Reply()
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+
+channel.start_consuming()
