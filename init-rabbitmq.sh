9353f379b96c018e1179b2ae6ceedc9a85807fc4
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-09 21:10:36 +0400

    Removed MQ init

--- init-rabbitmq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 10
-rabbitmqctl add_vhost demoAccess
-rabbitmqctl add_user remoteClient01 remoteClient01Pass
-rabbitmqctl set_permissions -p demoAccess remoteClient01 ".*" ".*" ".*"
