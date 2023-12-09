e459a06e0f8ec239e1aba96b53975c4bd20b025a
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-09 21:56:13 +0400

    Got rid of init file for RabbitMQ

--- initrabbitmq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 20
-
-wait_for_rabbitmq
-rabbitmqctl add_vhost demoAccess
-rabbitmqctl add_user remoteClient01 remoteClient01Pass
-rabbitmqctl set_permissions -p demoAccess remoteClient01 ".*" ".*" ".*"
