f5511987162099fa53b39c09a745d020fd04cb57
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-09 21:40:04 +0400

    nothing

--- initrabbitmq.sh
+++ initrabbitmq.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to wait for RabbitMQ server to start
 wait_for_rabbitmq() {
     echo "Waiting for RabbitMQ to start..."
     while ! rabbitmqctl status >/dev/null 2>&1; do
@@ -8,10 +7,7 @@ wait_for_rabbitmq() {
     done
 }
 
-# Wait for RabbitMQ server to start
 wait_for_rabbitmq
-
-# Add vhost, user, and set permissions
 rabbitmqctl add_vhost demoAccess
 rabbitmqctl add_user remoteClient01 remoteClient01Pass
 rabbitmqctl set_permissions -p demoAccess remoteClient01 ".*" ".*" ".*"
