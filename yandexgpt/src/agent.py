9efedabb76b95f47c09a02efce280202d1b9b552
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-11 01:56:30 +0300

    Bugfix YandexGPT

--- yandexgpt/src/agent.py
+++ yandexgpt/src/agent.py
@@ -43,7 +43,7 @@ def complete(prompt, strInput):
     }
     response = requests.post(url, headers=headers, json=prompt)
     result = response.text
-    return strOutput = result
+    return result
     
 
 def Reply(body):
