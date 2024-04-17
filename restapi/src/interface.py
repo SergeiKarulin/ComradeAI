808ba8dbcad300f5cc7f329f63fb31825fe838ce
sergey.karulin@gmail.com <sergey.karulin@gmail.com>
2024-04-17 15:44:53 -0700

    Updated REST-API, added webform for tests

--- restapi/src/interface.py
+++ restapi/src/interface.py
@@ -14,7 +14,7 @@ import json
 import os
 from PIL import Image
 from pydantic import BaseModel, validator
-from typing import List, Union, Dict, Optional
+from typing import Annotated, Dict, List, Optional, Union
 from tempfile import NamedTemporaryFile
 
 app = FastAPI(title="ComradeAI REST-API", version="0.18.19")
@@ -131,11 +131,6 @@ async def get_agent_response(request: MultiformatRequest):
         # elif content_type.startswith("video"):
         #     # Handle video file
         #     added_prompts.append(UnifiedPrompt(content_type="video", content=await file_obj.read(), mime_type=content_type))
-        # elif content_type in ["image/jpg", "image/png", "image/jpeg", "image/gif"]:
-        #     # Process image files
-        #     image_data = await file_obj.read()
-        #     image = Image.open(BytesIO(image_data))
-        #     added_prompts.append(UnifiedPrompt(content_type="image", content=image, mime_type=content_type))
         
         # elif content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
         #     # Process Excel files
@@ -202,8 +197,117 @@ async def get_agent_response(request: MultiformatRequest):
         return {"result": "error", "content": str(ex)}
 
     agent = Agent(AI, request.agentAddress)
-    # TODO. Чтобы это работало универсально, нужно составной токен и изменения в роутер
+    # TODO. To make it work properly, each oepration must create an own temprorary queue, which requires Router changes
     
+    resultDialog = request_dialog >> agent
+    
+    result["content"]["raw_response"] = json.loads(resultDialog.serialize())
+    
+    for message in resultDialog.messages:
+        for unified_prompt in message.unified_prompts:
+            if unified_prompt.content_type == 'text':
+                result["content"]["last_text_output"] = unifiedPromptToJSON(unified_prompt)
+            if unified_prompt.content_type == 'image':
+                # Original architecture mistake - the lib uses pillow images instead of binaries.
+                result["content"]["last_image_output"] = unifiedPromptToJSON(unified_prompt)
+            if unified_prompt.content_type == 'audio':
+                result["content"]["last_audio_output"] = unifiedPromptToJSON(unified_prompt)
+            if unified_prompt.content_type == 'video':
+                result["content"]["last_video_output"] = unifiedPromptToJSON(unified_prompt)
+            if unified_prompt.content_type == 'document':
+                result["content"]["last_document_output"] = unifiedPromptToJSON(unified_prompt)
+    
+    result["result"] = "success"
+    result = jsonable_encoder(result)
+    return result 
+
+@app.post("/get_agent_response_webform/")
+async def get_agent_response_webform(
+    agentAddress: str = Form(...),
+    text: Optional[str] = Form(None),
+    comradeAIToken: Optional[str] = Form(None),
+    requestAgentConfig: Optional[str] = Form("{}"),
+    audio_file: Annotated[UploadFile, File(description="Audio file: mp3, wav, or ogg")] = None,
+    image_file: Annotated[UploadFile, File(description="Image file: bmp, gif, png, jpg, jpeg, tif, tiff, or webp")] = None):
+
+    global comradeai_token
+
+    # Convert JSON string back to dictionary
+    try:
+        agent_config_dict = json.loads(requestAgentConfig)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400, detail="Invalid JSON format for agent config.")
+
+
+    # Self-expanatory result format
+    result = {
+        "result" : None, # Success when result is 200 OK. In case of error server returns 400 with error message.
+        "content": {    # Each output looks like {"content_type": "", "content": "", "mime_type: ""}
+            "last_text_output": None,
+            "last_image_output": None,
+            "last_audio_output": None
+        }
+    }
+    
+    # Validate that at least one type of content is present
+    if not text and not audio_file and not image_file:
+        raise HTTPException(status_code=400, detail="At least one of text, audio, image must be present")
+    
+    # Validate that the instance has the ComradeAI token to operate
+    if not comradeAIToken or len(comradeAIToken) == 0:
+        if comradeai_token and len(comradeai_token)>0:
+            local_comradeai_token = comradeai_token
+        else:
+            raise HTTPException(status_code=400, detail="Comrade AI token is not set")
+    else:
+        local_comradeai_token = comradeAIToken
+
+    # Process files if present
+    added_prompts = []
+    if text:
+        added_prompts.append(UnifiedPrompt(content_type="text", content=text, mime_type="text/plain"))
+               
+    agentConfig = None
+    if requestAgentConfig != {}:
+        agentConfig = requestAgentConfig
+
+    # Preparing prompt of audio_file
+    if audio_file:
+        audio_file_content = await audio_file.read()
+        audio_file_extension = audio_file.filename.split(".")[-1].lower()
+        print(f"{str(datetime.now())} Processing file of type {audio_file.filename}", flush=True)
+        if audio_file_extension in ["mp3", "wav", "ogg"]:
+            if audio_file_extension in ["wav", "ogg"]: mime_type = "audio/" + audio_file_extension 
+            else: mime_type = "audio/mpeg"
+            added_prompts.append(UnifiedPrompt(content_type="audio", content=audio_file_content, mime_type=mime_type))
+        else:
+            raise HTTPException(status_code=400, detail=f"Unsupported file type for audio file: {audio_file_extension}")
+        
+    # Preparing prompt of image_file
+    if image_file:
+        image_file_content = await image_file.read()
+        image_file_extension = image_file.filename.split(".")[-1].lower()
+        if image_file_extension in ["bmp", "gif", "png", "jpg", "jpeg", "tif", "tiff", "webp"]:
+            image_binary = BytesIO(image_file_content)
+            image = Image.open(image_binary)
+            mime_type = image_mime_types.get(image_file_extension, "application/octet-stream")
+            added_prompts.append(UnifiedPrompt(content_type="image", content=image, mime_type=mime_type))
+        else:
+            raise HTTPException(status_code=400, detail=f"Unsupported file type for image file: {image_file_extension}")
+   
+    # Shape as Mycelium Message and send to Mycelium
+    request_message = Message(role="user", unified_prompts=added_prompts, sender_info="REST-API User", subAccount="", routingStrategy=RoutingStrategy("direct", agentAddress))
+    request_dialog = None
+    request_dialog = Dialog(requestAgentConfig=agentConfig)
+    request_dialog.messages.append(request_message)
+    try:
+        AI = Mycelium(ComradeAIToken=local_comradeai_token)
+        AI.connect()
+    except Exception as ex:
+        print(f"{str(datetime.now())} Error: {str(ex)}", flush=True)
+        return {"result": "error", "content": str(ex)}
+
+    agent = Agent(AI, agentAddress)
     resultDialog = request_dialog >> agent
     
     result["content"]["raw_response"] = json.loads(resultDialog.serialize())
